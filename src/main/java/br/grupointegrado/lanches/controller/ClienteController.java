8afb1a83331599418153d359d3c4ec79ccd01049
Guilherme <guilhermenb19@gmail.com>
2024-11-11 20:41:55 -0300

    findbyid

--- src/main/java/br/grupointegrado/lanches/controller/ClienteController.java
+++ src/main/java/br/grupointegrado/lanches/controller/ClienteController.java
@@ -3,6 +3,8 @@ import br.grupointegrado.lanches.model.Cliente;
 import br.grupointegrado.lanches.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,12 +12,21 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/api/clientes")
+
 public class ClienteController {
+
     @Autowired
     private ClienteRepository repository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> findAll() {
         return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente nao encontrado"));
     }
 }
